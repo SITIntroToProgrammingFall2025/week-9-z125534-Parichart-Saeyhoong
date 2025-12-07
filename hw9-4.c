3b57d2331b1eec7bfb16632533791468b7c6431e
z125534-Parichart-Saeyhoong <z125534@shibaura-it.ac.jp>
2025-12-07 23:24:32 +0900

    Update hw9-4.c

--- hw9-4.c
+++ hw9-4.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int r, g, b;
+
+    scanf("%d", &r);
+    scanf("%d", &g);
+    scanf("%d", &b);
+
+    printf("The hex code is #%02X%02X%02X", r, g, b);
+
+    return 0;
+}
 
