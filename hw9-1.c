7a45fbe6c276c7b1d4cbc11f232be2429b9658ce
z125534-Parichart-Saeyhoong <z125534@shibaura-it.ac.jp>
2025-12-07 20:18:56 +0900

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a[4];
+    double answer;
+    scanf("%d", &a[0]);
+    scanf("%d", &a[1]);
+    scanf("%d", &a[2]);
+    scanf("%d", &a[3]);
+    answer = a[0]*a[3] - a[1]*a[2];
+    
+    printf("%.6f",answer);
+    return 0;
+}
 
