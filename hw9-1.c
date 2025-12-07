544fdad684d53236f3643ab279e0f21ba1ba819d
z125534-Parichart-Saeyhoong <z125534@shibaura-it.ac.jp>
2025-12-07 20:25:12 +0900

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -7,9 +7,12 @@ int main() {
     scanf("%d", &a[1]);
     scanf("%d", &a[2]);
     scanf("%d", &a[3]);
+
     answer = a[0]*a[3] - a[1]*a[2];
-    
-    printf("%.6f",answer);
+
+    printf("You entered\n");
+    printf("%d %d\n", a[0],a[1]);
+    printf("%d %d\n", a[2],a[3]);
+    printf("Determinant is %.6f",answer);
     return 0;
 }
-
