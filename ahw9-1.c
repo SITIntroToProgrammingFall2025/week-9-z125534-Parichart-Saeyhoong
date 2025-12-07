a57339bf9571aea9d6d78bc077b576b92c6b5785
z125534-Parichart-Saeyhoong <z125534@shibaura-it.ac.jp>
2025-12-07 23:32:39 +0900

    Update ahw9-1.c

--- ahw9-1.c
+++ ahw9-1.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+
+int main(void) {
+    int r, g, b;
+
+    scanf("%d", &r);
+    scanf("%d", &g);
+    scanf("%d", &b);
+
+    
+    int colors[5][3] = {
+        {0, 0, 0},       
+        {255, 0, 0},     
+        {0, 255, 0},     
+        {0, 0, 255},     
+        {255, 255, 255}  
+    };
+
+    char *names[5] = {
+        "Black", "Red", "Green", "Blue", "White"
+    };
+
+    int minDist = 999999999;
+    int nearest = 0;
+
+    for (int i = 0; i < 5; i++) {
+        int dr = r - colors[i][0];
+        int dg = g - colors[i][1];
+        int db = b - colors[i][2];
+
+        int dist = dr*dr + dg*dg + db*db;
+
+        if (dist < minDist) {
+            minDist = dist;
+            nearest = i;
+        }
+    }
+
+    printf("The nearest color is %s", names[nearest]);
+
+    return 0;
+}
+
 
